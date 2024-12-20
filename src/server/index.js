dfea74e5ff6c1273ba7fe55318c1de34f3e41d3a
tymey <tyler.meyer.a@gmail.com>
2024-12-20 15:45:31 -0600

    Create express server, start with npm start

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,9 @@
 const database = require('./database');
+const { app } = require('./app');
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://127.0.0.1:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
