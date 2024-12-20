97755d61e3841c35a2a85b8ca50c1ec8f2f5d0c3
evan.loria4 <evan.loria4@gmail.com>
2024-12-20 11:24:55 -0600

    Initialize webpack with rules and presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SRC_DIR = path.resolve(__dirname, 'src/client');
+const DIST = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'index.jsx'),
+  output: {
+    path: DIST,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(SRC_DIR, 'index.html'),
+      filename: 'index.html'
+    })
+  ],
+  watch: true
+};
