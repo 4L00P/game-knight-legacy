53753cbf7db3e62b5857cd08b8bee9a680c23609
evan.loria4 <evan.loria4@gmail.com>
2024-12-21 19:40:04 -0600

    Initialize Games model schema

--- src/server/database/models/Games.js
+++ src/server/database/models/Games.js
@@ -8,6 +8,9 @@ const GameSchema = {
   maxPlayers: { type: Number },
   playTime: { type: Number },
   minAge: { type: Number },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  notes: { types: String, default: '' },
+  rating: { type: Number, default: 0 },
 };
 
 const Games = mongoose.model('Game', new mongoose.Schema(GameSchema));
