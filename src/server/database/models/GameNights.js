cd075e2e2acf2855016e631c682e116c60f4a3ab
evan.loria4 <evan.loria4@gmail.com>
2024-12-21 19:39:47 -0600

    Initialize GameNights model schema

---
+++ src/server/database/models/GameNights.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const GameNightSchema = {
+  name: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  date: { type: Date, required: true },
+  isComplete: { type: Boolean, default: false },
+  isCancelled: { type: Boolean, default: false },
+  guests: { type: [String] },
+  snacks: { type: [String] },
+  games: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
+};
+
+const GameNights = mongoose.model('GameNight', new mongoose.Schema(GameNightSchema));
+
+module.exports = {
+  GameNights,
+};
