5367590646f99aee2ab76a46b6bf2d7e8cc3b6c0
evan.loria4 <evan.loria4@gmail.com>
2024-12-21 19:39:17 -0600

    Import and export al models

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,9 +1,20 @@
 const { connect } = require('mongoose');
+const { Users } = require('./models/Users');
+const { Games } = require('./models/Games');
+const { Groups } = require('./models/Groups');
+const { GameNights } = require('./models/GameNights');
 
 connect('mongodb://127.0.0.1:27017/game-knight')
   .then(() => {
-    console.log('Connected to Database')
+    console.log('Connected to Database');
   })
   .catch((err) => {
     console.error('Failed to connect to MongoDB:', err);
   });
+
+module.exports = {
+  Users,
+  Games,
+  Groups,
+  GameNights,
+};
