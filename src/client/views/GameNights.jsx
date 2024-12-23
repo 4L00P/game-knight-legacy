2273d2929513ba30b961aac525f77eda543d758c
evan.loria4 <evan.loria4@gmail.com>
2024-12-23 16:37:12 -0600

    Initialize Gamenight view

---
+++ src/client/views/GameNights.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GameNight() {
+  return (
+    <div>
+      <ul>
+        <li>Upcoming Game Night First</li>
+        <li>Any past Game Nights After</li>
+      </ul>
+    </div>
+  );
+}
+
+export default GameNight;
