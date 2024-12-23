71d689ad445db7d87dfc8119674810c1f990d870
evan.loria4 <evan.loria4@gmail.com>
2024-12-23 14:38:18 -0600

    Create Navbar component with one button

---
+++ src/client/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Stack,
+  Button,
+} from '@mui/material';
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Game Knight
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Game Nights</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
