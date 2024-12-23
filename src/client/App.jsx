f8ed0ad0be7a0d0b86d50c4c636ac2e959b96e89
evan.loria4 <evan.loria4@gmail.com>
2024-12-23 17:20:47 -0600

    Add nested 'gamenights' route in 'home' route

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Hello from './views/Home.jsx';
 import Login from './views/Login.jsx';
+import GameNights from './views/GameNights.jsx';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="home" element={<Hello />} />
+        <Route path="home" element={<Hello />}>
+          <Route path="gamenights" element={<GameNights />} />
+        </Route>
       </Routes>
     </div>
   );
