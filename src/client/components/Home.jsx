484809071f7a9842475e44e4700cfc973e39a042
evan.loria4 <evan.loria4@gmail.com>
2024-12-23 15:20:07 -0600

    Add Hello.jsx contents to file

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function Home() {
   return (
-  <h1>Home</h1>,
-)
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
