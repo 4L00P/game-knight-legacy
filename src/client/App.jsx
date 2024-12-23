013392d363307d713f4c155db1db1871c0ba7243
evan.loria4 <evan.loria4@gmail.com>
2024-12-23 14:38:43 -0600

    Render Navbar component at the top of App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,13 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 
 import Hello from './components/Hello.jsx';
 import Login from './views/Login.jsx';
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Hello />} />
-      <Route path="login" element={<Login />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
