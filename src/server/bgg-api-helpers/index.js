575403ffecf0ca564d19b63c033daad27369e714
tymey <tyler.meyer.a@gmail.com>
2024-12-26 10:05:17 -0600

    getGameInfoByID helper to parse thru res from BGG

--- src/server/bgg-api-helpers/index.js
+++ src/server/bgg-api-helpers/index.js
@@ -17,8 +17,10 @@ const convertXML = (xmlData) => (
  * I: XML data converted into a JavaScript object.
  * O: An object with meaningful keys for Database storage.
  * C: N/A
- * E: May not have certain keys such as bestWith & recommendedWith
+ * E: May not have certain keys such as bestWith & recommendedWith.
+ *    Use a conditional to deal with this.
  */
+// Synchronous function
 const buildGameObj = (convertedData) => {
   // Destructure important keys
   const {
@@ -36,6 +38,8 @@ const buildGameObj = (convertedData) => {
     minage,
   } = convertedData.items.item[0];
 
+  console.log(convertedData.items.item[0]['poll-summary'][0].result);
+
   // Build game info object
   const bggObj = {
     id: +($.id),
@@ -47,31 +51,48 @@ const buildGameObj = (convertedData) => {
     yearPublished: +(yearpublished[0].$.value),
     minPlayers: +(minplayers[0].$.value),
     maxPlayers: +(maxplayers[0].$.value),
-
-    // // May need to drop this --- vvvv
-    // bestWith: (
-    //   !(convertedData['poll-summary']) ? null :
-    //   !(convertedData['poll-summary'][0].result) ? null :
-    //   convertedData['poll-summary'][0].result[0].$.value
-    // ),
-    // recommendedWith: (
-    //   !(convertedData['poll-summary']) ? null :
-    //   !(convertedData['poll-summary'][0].result) ? null :
-    //   convertedData['poll-summary'][0].result[1].$.value
-    // ),
-    // // May need to drop this --- ^^^^
-
     playingTime: +(playingtime[0].$.value),
     minPlayTime: +(minplaytime[0].$.value),
     maxPlayTime: +(maxplaytime[0].$.value),
     minimumAge: +(minage[0].$.value),
   };
 
+  // If there are any poll results with additional info, they'll be in this array
+  const pollSummaryArray = convertedData.items.item[0]['poll-summary'][0].result;
+
+  // If it is an array, iterate through it and create a key for each category
+  if (Array.isArray(pollSummaryArray)) {
+    pollSummaryArray.forEach((category) => {
+      bggObj[category.$.name] = category.$.value;
+    });
+  }
+
   // Return the object
   return bggObj;
 };
 
+/*
+Retrieves and build game object from Board Game Geek (async)
+*/
+const getGameInfoByID = (id) => {
+  axios.get(`https://boardgamegeek.com/xmlapi2/thing?id=${id}`)
+    // Destructure response from BGG to grab XML from the data key
+    // convertXML returns a promise; implicit return to the next .then()
+    .then(({ data }) => convertXML(data))
+    .then((result) => {
+      const gameInfoObj = buildGameObj(result);
+      console.log('BGG GAME OBJECT:', gameInfoObj);
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+    });
+};
+
+// Testing getGameInfoByID
+getGameInfoByID(293739); // Expect info about EL: The Chicago Transit Adventure
+
 module.exports = {
   convertXML,
   buildGameObj,
+  getGameInfoByID,
 };
