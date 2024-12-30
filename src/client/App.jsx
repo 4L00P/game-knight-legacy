74a48363a4fc7bd4ea43f1daed76e7eb28fdf1cb
tymey <tyler.meyer.a@gmail.com>
2024-12-30 09:31:27 -0600

    Change Hello component to Home

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Hello from './views/Home';
+import Home from './views/Home';
 import Login from './views/Login';
 import GameNights from './views/GameNights';
 
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="home" element={<Hello />} />
+        <Route path="home" element={<Home />} />
         <Route path="gamenights" element={<GameNights />} />
       </Routes>
     </div>
