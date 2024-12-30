fd24c761b523ae4e4a5e6ca35da5108b8e7a0b26
tymey <tyler.meyer.a@gmail.com>
2024-12-30 11:38:24 -0600

    Add proptypes check for games in GamesList

--- src/client/components/GamesList.jsx
+++ src/client/components/GamesList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { List } from '@mui/material';
 
 import Game from './Game';
@@ -6,14 +7,25 @@ import Game from './Game';
 function GamesList({ games }) {
   return (
     <List>
-      {games.map((game) => (
-        <Game
-          key={game._id}
-          game={game}
-        />
-      ))}
+      {games.map((game) => {
+        const { _id } = game;
+        return (
+          <Game
+            key={_id}
+            game={game}
+          />
+        );
+      })}
     </List>
   );
 }
+
+GamesList.propTypes = {
+  games: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default GamesList;
