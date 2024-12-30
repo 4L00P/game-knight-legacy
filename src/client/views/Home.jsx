f2d77a628e0a014eed0cb9a1792a6f1e104b7092
tymey <tyler.meyer.a@gmail.com>
2024-12-30 09:32:23 -0600

    Add state var name to Home view

--- src/client/views/Home.jsx
+++ src/client/views/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
-function Hello() {
+const { useState } = React;
+
+function Home() {
+  const [name, setName] = useState('');
   return (
     <div>
       <Navbar />
       <h1>Hello World</h1>
+      <input
+        value={name}
+        onChange={(e) => { setName(e.target.value); }}
+      />
     </div>
   );
 }
 
-export default Hello;
+export default Home;
