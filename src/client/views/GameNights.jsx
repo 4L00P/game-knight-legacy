cb720ddee9cff68d687067cef4e7c21fc7c1e7c6
evan.loria4 <evan.loria4@gmail.com>
2024-12-30 10:07:19 -0600

    Add button for creating, implement conditional rendering

--- src/client/views/GameNights.jsx
+++ src/client/views/GameNights.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 import Navbar from '../components/Navbar';
 
 function GameNights() {
+  const [creatingNight, toggleCreatingNight] = useState(false);
+
+  const handleClick = () => {
+    console.log('Toggling Creatingnight state');
+    toggleCreatingNight(true);
+  };
   return (
     <div>
       <Navbar />
@@ -9,6 +17,15 @@ function GameNights() {
         <li>Upcoming Game Night First</li>
         <li>Any past Game Nights After</li>
       </ul>
+      {!creatingNight && (
+        <Button
+          variant="contained"
+          onClick={handleClick}
+        >
+          Schedule Game Night
+        </Button>
+      )}
+      {creatingNight && <p>Creating Night</p>}
     </div>
   );
 }
