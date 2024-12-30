0b57244bdd038d67f4bf9b7050803763cc676e88
evan.loria4 <evan.loria4@gmail.com>
2024-12-30 14:13:38 -0600

    Render form with input fields

--- src/client/components/CreateNight.jsx
+++ src/client/components/CreateNight.jsx
@@ -13,32 +13,45 @@ function GameNightForm() {
   // Initialize the state of the component
   const [name, setName] = useState('');
   const [date, setDate] = useState('');
-  const [guests, setGuests] = useState([]);
-  const [snacks, setSnacks] = useState([]);
-  const [games, setGames] = useState([]);
+  const [guests, setGuests] = useState([{ label: 'Guest', value: '', helperText: 'Who\'s invited?' }]);
+  const [snacks, setSnacks] = useState([{
+    label: 'Snack', type: 'text', value: '', helperText: 'What\'s on the menu?',
+  }]);
+  const [games, setGames] = useState([{ label: 'Game', value: '', helperText: 'What are we playing?' }]);
 
-  // Helper function to map over collection and return  InputField
-  const mapCollection(stateValue)
   // Function that handles the change of the form input fields
   /**
    * I: element and index
    */
   const handleChange = (element, index) => {
-    const values = [...snacks];
-    console.log('Values: ', values, index);
-    values[index].value = element.target.value;
-    setFormValues(values);
+
   };
-  console.log('Form Values:', formValues);
+  // Helper function to map over collection and return  InputField
+  const mapCollection = (stateValue) => {
+    console.log('Mapping: ', stateValue);
+    return stateValue.map((obj, index) => (
+      <InputField
+        key={`${obj.value + index}`}
+        index={index}
+        objvalue={obj}
+        onChange={handleChange}
+      />
+    ));
+  };
+
   return (
     <FormControl>
-      {formValues.map((obj, index) => (
-        <InputField
-          key={`${obj.label + index}`}
-          objvalue={obj}
-          onChange={(element) => { handleChange(element, index); }}
-        />
-      ))}
+      <TextField
+        type="text"
+        label="Name"
+        id="name"
+        variant="outlined"
+        helperText="Name your event"
+        onChange={handleChange}
+      />
+      {mapCollection(guests)}
+      {mapCollection(snacks)}
+      {mapCollection(games)}
     </FormControl>
   );
 }
