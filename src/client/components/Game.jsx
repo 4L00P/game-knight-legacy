4a3f19b0b7dfc36be04fa65bcf8cbdcc56d79234
tymey <tyler.meyer.a@gmail.com>
2024-12-30 11:40:48 -0600

    Add avatar to Game comp, check proptypes

--- src/client/components/Game.jsx
+++ src/client/components/Game.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { ListItem } from '@mui/material';
+import PropTypes from 'prop-types';
+import {
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+} from '@mui/material';
 
 function Game({ game }) {
+  const { name, thumbnail } = game;
   return (
     <ListItem>
-      {game.name}
+      <ListItemAvatar>
+        <Avatar src={thumbnail} />
+      </ListItemAvatar>
+      <ListItemText>
+        {name}
+      </ListItemText>
     </ListItem>
   );
 }
+
+Game.propTypes = {
+  game: PropTypes.shape({
+    name: PropTypes.string,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+};
 
 export default Game;
