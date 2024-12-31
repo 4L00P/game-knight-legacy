598ffe5daf5536804f16a3593a81bb3bc114c047
tymey <tyler.meyer.a@gmail.com>
2024-12-31 12:06:19 -0600

    Set up login paths for Google Sign In

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,6 +1,31 @@
 const { Router } = require('express');
+const passport = require('passport');
+require('./auth-passport');
 
 const authRouter = Router();
+
+authRouter.get('/', passport.authenticate('google', {
+  scope: ['email', 'profile'],
+}));
+
+authRouter.get('/callback', passport.authenticate('google', {
+  successRedirect: 'auth/success',
+  failureRedirect: 'auth/failure',
+}));
+
+// Success
+authRouter.get('/success', (req, res) => {
+  if (!req.user) {
+    res.redirect('/auth/failure');
+  } else {
+    res.redirect('/home');
+  }
+});
+
+// Failure
+authRouter.get('/failure', (req, res) => {
+  res.redirect('/');
+});
 
 module.exports = {
   authRouter,
