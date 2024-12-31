ccefb9d09cdd94ecdabfa13c6f711bbecd7d78d3
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 17:35:40 -0600

    Create DividedListItem component file

---
+++ src/client/components/DividedListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  ListItem,
+  ListItemText,
+  Typography,
+  Divider,
+} from '@mui/material';
+
+function DividedListItem({ element }) {
+
+}
+
+export default DividedListItem;
