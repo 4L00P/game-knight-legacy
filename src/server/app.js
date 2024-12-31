3bbd054b1ea6775260c3acfe48c7c08da217d35e
tymey <tyler.meyer.a@gmail.com>
2024-12-31 15:56:23 -0600

    Handle GET /logout to logout user, destroy cookie

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+require('./routes/auth-passport');
 
 const {
   authRouter,
@@ -10,12 +11,35 @@ const {
   gameNightsRouter,
 } = require('./routes');
 
+const { SESSION_SECRET } = process.env;
+
 const app = express();
 
 const DIST_DIR = path.resolve(__dirname, '..', '..', 'dist');
 
+app.all('*', (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(express.static(DIST_DIR));
 app.use(express.json());
+
+// Session
+app.use(session({
+  name: 'google-auth-session',
+  secret: SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    // The cookie will last one hour.
+    maxAge: 60000 * 60,
+  },
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routers
 app.use('/auth', authRouter);
@@ -23,10 +47,22 @@ app.use('/api/games', gamesRouter);
 app.use('/api/groups', groupsRouter);
 app.use('/api/game-nights', gameNightsRouter);
 
+app.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+      res.sendStatus(500);
+    } else {
+      res.clearCookie('google-auth-session');
+      res.redirect('/');
+    }
+  });
+});
+
 /**
  * If React-Router sends a request for a particular webpage, send the index.html in response
  */
-
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root: DIST_DIR });
 });
