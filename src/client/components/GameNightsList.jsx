c256c73aac2ea62cc845fa1fa6157aa095aab178
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 14:52:12 -0600

    Render GameNight names dynamically

--- src/client/components/GameNightsList.jsx
+++ src/client/components/GameNightsList.jsx
@@ -4,6 +4,9 @@ import { List } from '@mui/material';
 function GameNightsList({ gameNights, getGameNights }) {
   return (
     <List>
+      {gameNights.map((night) => (
+        <p>{night.name}</p>
+      ))}
       Nights List
     </List>
   );
