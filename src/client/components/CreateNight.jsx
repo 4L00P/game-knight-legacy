9b8971e233c486d4fc7a3ad9ad079fc8fefcf1ad
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 15:27:20 -0600

    Add propTypes

--- src/client/components/CreateNight.jsx
+++ src/client/components/CreateNight.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import {
   FormControl,
   InputLabel,
@@ -148,5 +149,9 @@ function GameNightForm({ closeForm }) {
     </Box>
   );
 }
+
+GameNightForm.propTypes = {
+  closeForm: PropTypes.func.isRequired,
+};
 
 export default GameNightForm;
