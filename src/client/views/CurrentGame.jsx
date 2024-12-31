fdddd50d510996549ee33403df5a14cda995cbc4
stephen <jimisax@gmail.com>
2024-12-31 17:09:30 -0600

    ADD working paper components to currentGame

--- src/client/views/CurrentGame.jsx
+++ src/client/views/CurrentGame.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Stack,
+import { useState } from 'react';
+
+import {
+  Stack,
   Typography,
   Paper,
   ListItem
 } from '@mui/material';
-// import{ Grid } from '@mui/material/grid2';
-import Navbar from '../components/Navbar';
-import { name, members } from '../../server/database/models/Groups';
 
-const { useState, useEffect } = React;
+import Navbar from '../components/Navbar';
+// import { name, members } from '../../server/database/models/Groups';
 
 function CurrentGame() {
+  // tie members to state so we can change based on cancellations and removals. give a different name
+  // [ players, SetPlayers ] = useState([]);
+  // [ isTurn, SetIsTurn ] = useState(false);
+  // tie a variable to state that signifies whether it is that player's turn
   /**
    * Game Needs:
    * 1. Array of strings representing the names of each person playing(members);
@@ -21,15 +26,16 @@ function CurrentGame() {
    * 3. a timeout form and function that says when to alert a player that they're taking too long
    *  (maybe toggle the consequences. alert, move to next player);
    */
-  // tie members to state so we can change based on cancellations and removals
-  [ members, SetMembers ] = useState;
-  // tie a variable to state that signifies whether it is that player's turn
-  [ isTurn, SetIsTurn ] = useState;
   // when you press start game, you get a filter with all gamenights set for today.
   // Picking one(even if there's only one), starts the get/post process.
   // set the middle player indicators(with vp/hp) on a Paper surface.
-  // Have the elevation change from 1 to 24 depending on whether it is their turn
   // add square corners <paper square>person</paper> or square=`${true}`
+  // make the papers vertical, not horizontal. and long, possibly
+  // Have the elevation change from 1 to 24 depending on whether it is their turn
+    // if it's their turn, crank the padding up to 16 or so, the elevation to 24,
+    // and the padding color to something shimmery
+    // when it's not their turn, padding back down to 0, elevation to 1
+
 
   return (
     <div>
@@ -37,18 +43,24 @@ function CurrentGame() {
       <Navbar />
       <h1>TEST GAME!!!</h1>
       <div className="turn-order-tags">
-        <span>John</span>
-        <span>Jacob</span>
-        <span>Jingleheimer</span>
-        <span>Schmidt</span>
+        <div>John</div>
+        <div>Jacob</div>
+        <div>Jingleheimer</div>
+        <div>Schmidt</div>
       </div>
-      <div className="player-info-bars">
-        <Paper elevation={0}>John info</Paper>
+       <div className="player-info-bars">
+        <Paper elevation={0}
+        // sx={
+        //   padding: 2,
+        //   border: "1px solid black",
+        //   margin: 8
+        // }
+        >John info</Paper>
         <Paper elevation={8}>Jacob info</Paper>
         <Paper elevation={16}>Jingle info</Paper>
-        <Paper elevation={24}>Schmidt  info</Paper>
+        <Paper elevation={24}>Schmidt info</Paper>
 
-      </div>
+      </div> 
 
     </div>
   );
