b710e0141f888230046e412f5a825652b442d15f
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 15:54:11 -0600

    Create component for details of a gameNight

---
+++ src/client/components/NightDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AccordionDetails } from '@mui/material';
+
+function NightDetails() {
+  return (
+    <AccordionDetails>
+
+    </AccordionDetails>
+  );
+};
+
+export default NightDetails;
