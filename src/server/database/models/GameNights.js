a2dcfff56f0ce41d11a09bc43b23a143724f79fb
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 10:33:07 -0600

    Push to open PR and merge

--- src/server/database/models/GameNights.js
+++ src/server/database/models/GameNights.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const GameNightSchema = {
   name: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  date: { type: Date, required: true },
+  date: { type: Date, required: false, default: new Date() },
   isComplete: { type: Boolean, default: false },
   isCancelled: { type: Boolean, default: false },
   guests: [{ type: String }],
