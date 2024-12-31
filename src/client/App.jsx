3e76a9e74a0899e2ec5a373e1d39e6a029e123fb
stephen <jimisax@gmail.com>
2024-12-31 15:08:20 -0600

    ADD currentgames route to App.js ADD hardcode testing for CurrentGame.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import CurrentGame from './views/CurrentGame';
 import Home from './views/Home';
 import Login from './views/Login';
 import GameNights from './views/GameNights';
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="gamenights" element={<GameNights />} />
+        <Route path="current-game" element={<CurrentGame />} />
       </Routes>
     </div>
   );
--- src/client/views/CurrentGame.jsx
+++ src/client/views/CurrentGame.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Stack,
+  Typography,
+  Paper,
+  ListItem
+} from '@mui/material';
+// import{ Grid } from '@mui/material/grid2';
 import Navbar from '../components/Navbar';
 import { name, members } from '../../server/database/models/Groups';
 
-function CurrentGame(props) {
+const { useState, useEffect } = React;
+
+function CurrentGame() {
   /**
    * Game Needs:
    * 1. Array of strings representing the names of each person playing(members);
@@ -13,18 +21,35 @@ function CurrentGame(props) {
    * 3. a timeout form and function that says when to alert a player that they're taking too long
    *  (maybe toggle the consequences. alert, move to next player);
    */
-  [ members, SetMembers ] = useState
-  // when you press start game, you get a filter with all gamenights set for today. Picking one(even if there's only one), starts the get/post process.
+  // tie members to state so we can change based on cancellations and removals
+  [ members, SetMembers ] = useState;
+  // tie a variable to state that signifies whether it is that player's turn
+  [ isTurn, SetIsTurn ] = useState;
+  // when you press start game, you get a filter with all gamenights set for today.
+  // Picking one(even if there's only one), starts the get/post process.
+  // set the middle player indicators(with vp/hp) on a Paper surface.
+  // Have the elevation change from 1 to 24 depending on whether it is their turn
+  // add square corners <paper square>person</paper> or square=`${true}`
 
   return (
     <div>
+      <h1>Game Knight Active</h1>
       <Navbar />
-      <h1>{`${name}!!`}</h1>
-      {members.map((member) => {
-        return(
+      <h1>TEST GAME!!!</h1>
+      <div className="turn-order-tags">
+        <span>John</span>
+        <span>Jacob</span>
+        <span>Jingleheimer</span>
+        <span>Schmidt</span>
+      </div>
+      <div className="player-info-bars">
+        <Paper elevation={0}>John info</Paper>
+        <Paper elevation={8}>Jacob info</Paper>
+        <Paper elevation={16}>Jingle info</Paper>
+        <Paper elevation={24}>Schmidt  info</Paper>
 
-        );
-      })}
+      </div>
+
     </div>
   );
 }
