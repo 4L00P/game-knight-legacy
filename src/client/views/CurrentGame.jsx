3094dfdb8abf9679aa7bef2d0270568998c086d0
stephen <jimisax@gmail.com>
2024-12-31 19:58:15 -0600

    ADD light css to paper components

--- src/client/views/CurrentGame.jsx
+++ src/client/views/CurrentGame.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-import {
-  Stack,
-  Typography,
-  Paper,
-  ListItem
-} from '@mui/material';
+import { Stack, Typography, Paper, ListItem } from "@mui/material";
 
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 // import { name, members } from '../../server/database/models/Groups';
 
 function CurrentGame() {
-  // tie members to state so we can change based on cancellations and removals. give a different name
+  // tie members to state so we can change based on cancellations and removals.
   // [ players, SetPlayers ] = useState([]);
-  // [ isTurn, SetIsTurn ] = useState(false);
   // tie a variable to state that signifies whether it is that player's turn
+  // [ isTurn, SetIsTurn ] = useState(false);
   /**
    * Game Needs:
    * 1. Array of strings representing the names of each person playing(members);
@@ -32,33 +27,35 @@ function CurrentGame() {
   // add square corners <paper square>person</paper> or square=`${true}`
   // make the papers vertical, not horizontal. and long, possibly
   // Have the elevation change from 1 to 24 depending on whether it is their turn
-    // if it's their turn, crank the padding up to 16 or so, the elevation to 24,
-    // and the padding color to something shimmery
-    // when it's not their turn, padding back down to 0, elevation to 1
+  // if it's their turn, crank the padding up to 16 or so, the elevation to 24,
+  // and the padding color to something shimmery
+  // when it's not their turn, padding back down to 0, elevation to 1
 
   // use set interval to change the game knight colors
 
   return (
     <div>
-      <Typography variant="h2" align="center">Game Knight Active</Typography>
+      <Typography variant="h2" align="center">
+        Game Knight Active
+      </Typography>
       <Navbar />
-      <Typography variant="h2" align="center">TEST GAME!!!</Typography>
+      <Typography variant="h2" align="center">
+        TEST GAME!!!
+      </Typography>
       {/* <div className="turn-order-tags">
         <div>John</div>
         <div>Jacob</div>
         <div>Jingleheimer</div>
         <div>Schmidt</div>
       </div> */}
-       <div className="player-info-bars">
-        <Paper elevation={0}
-        height='20px'
-        >John info</Paper>
-        <Paper elevation={8} >Jacob info</Paper>
+      <div className="player-info-bars">
+        <Paper elevation={0} style={{ height: '20px', width: '90px' }}>
+          John info
+        </Paper>
+        <Paper elevation={8}>Jacob info</Paper>
         <Paper elevation={16}>Jingle info</Paper>
         <Paper elevation={24}>Schmidt info</Paper>
-
       </div>
-
     </div>
   );
 }
