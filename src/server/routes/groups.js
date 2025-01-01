6e5a3cbf1f324caa74a15bd00efd94b6a01c6e0f
stephen <jimisax@gmail.com>
2025-01-01 16:32:38 -0600

    ADD post request handling for groups.js

--- src/server/routes/groups.js
+++ src/server/routes/groups.js
@@ -9,8 +9,27 @@ const groupsRouter = Router();
 /**
  * Set a post request for starting an empty group
  */
-Router.get();
+groupsRouter.post('/', (req, res) => {
+  // the way we decided this, the request body should be blank, so no info to hold
+  // just create an empty Groups schema to hold the group
+  Groups.create().then(() => {
+    // send a success response 201
+    res.status(201).send('Successfully created group');
+  }).catch((err) => {
+    // if caught, log that there's an issue, and what the issue is
+    console.error('Could Not Post Group', err);
+    // and add a response 500
+    res.sendStatus(500);
+  });
+});
 
+/**
+ * Set a patch request to amend new groups schema
+ */
+groupsRouter.patch('/', (req, res) => {
+  const info = req.body;
+  
+});
   module.exports = {
     groupsRouter,
   };
