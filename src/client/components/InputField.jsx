d110d91dcefec9aef659bb73d43bd0cdaf1710b4
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 18:01:56 -0600

    Render list elements to page as they are added to collection

--- src/client/components/InputField.jsx
+++ src/client/components/InputField.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { TextField, Button } from '@mui/material';
 import PropTypes from 'prop-types';
 
-function InputField({ objvalue, handleChange, handleAddClick }) {
+function InputField({
+  objvalue,
+  handleChange,
+  formValues,
+  handleAddClick,
+  createDividedList,
+}) {
   const {
     label,
     value,
@@ -30,6 +36,7 @@ function InputField({ objvalue, handleChange, handleAddClick }) {
         </Button>
       )
       }
+      {formValues[collection].length ? createDividedList(formValues[collection]) : null}
     </div>
   );
 }
@@ -41,8 +48,14 @@ InputField.propTypes = {
     collection: PropTypes.string,
     helperText: PropTypes.string,
   }).isRequired,
+  formValues: PropTypes.shape({
+    guests: PropTypes.arrayOf(PropTypes.string),
+    snacks: PropTypes.arrayOf(PropTypes.string),
+    games: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
   handleChange: PropTypes.func.isRequired,
   handleAddClick: PropTypes.func.isRequired,
+  createDividedList: PropTypes.func.isRequired,
 };
 
 export default InputField;
