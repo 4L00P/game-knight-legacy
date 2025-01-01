3686ea9da5f91f5ed944b8fba1d8327ec5a30b34
evan.loria4 <evan.loria4@gmail.com>
2024-12-31 18:08:56 -0600

    Add divider and ListItemText component

--- src/client/components/DividedListItem.jsx
+++ src/client/components/DividedListItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import {
   ListItem,
   ListItemText,
@@ -7,7 +8,18 @@ import {
 } from '@mui/material';
 
 function DividedListItem({ element }) {
-
+  return (
+    <div>
+      <ListItem>
+        <ListItemText primary={element} />
+      </ListItem>
+      <Divider component="li" />
+    </div>
+  );
 }
+
+DividedListItem.propTypes = {
+  element: PropTypes.string.isRequired,
+};
 
 export default DividedListItem;
