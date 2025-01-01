a393d9f66b1bfcd20662052fff17cf82cc700ac0
tymey <tyler.meyer.a@gmail.com>
2025-01-01 10:23:59 -0600

    Remove console log from GET /auth

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -6,9 +6,7 @@ const authRouter = Router();
 
 authRouter.get('/', passport.authenticate('google', {
   scope: ['email', 'profile'],
-}), (req, res) => {
-  console.log('hey');
-});
+}));
 
 authRouter.get('/callback', passport.authenticate('google', {
   successRedirect: '/auth/success',
