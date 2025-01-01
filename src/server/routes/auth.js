412e85d55a3c261bffa20a19833b773ce4dd14f9
tymey <tyler.meyer.a@gmail.com>
2025-01-01 11:03:08 -0600

    Add notes to auth router file

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -4,16 +4,31 @@ require('./auth-passport');
 
 const authRouter = Router();
 
+/*
+GET /auth => Used to serve the client a Google Login page
+  - Grabs email and profile from Google info on success login
+  - Needs to served with href="/auth" for client to be redirected to Google Login
+*/
 authRouter.get('/', passport.authenticate('google', {
   scope: ['email', 'profile'],
 }));
 
+/*
+GET /auth/callback => Used to catch Google's response from login
+  - Needs to be set up in the Google Strategy for passport.js
+  - Need to authorize /auth/callback redirect URI in OAuth token
+  - Redirect to the proper route on success or failure
+*/
 authRouter.get('/callback', passport.authenticate('google', {
   successRedirect: '/auth/success',
   failureRedirect: '/auth/failure',
 }));
 
-// Success
+/*
+GET /auth/success => On a successful login with Google, check for a user on request"
+  - If there is no user, redirect to /auth/failure
+  - If there is a user, redirect to /home for client to start using app
+*/
 authRouter.get('/success', (req, res) => {
   if (!req.user) {
     res.redirect('/auth/failure');
@@ -22,7 +37,9 @@ authRouter.get('/success', (req, res) => {
   }
 });
 
-// Failure
+/*
+GET /auth/failure => On a failed login with Google, send the user back to the landing page
+*/
 authRouter.get('/failure', (req, res) => {
   res.redirect('/');
 });
