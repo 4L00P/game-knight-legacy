d939c5e161c2bd3a01a0f7e8d265ac36cf8d2207
stephen <jimisax@gmail.com>
2025-01-02 10:02:21 -0600

    ADD minor comments to Groups.jsx

--- src/client/views/Groups.jsx
+++ src/client/views/Groups.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from 'axios';
 
 const Groups = () => {
-// 
+// This'll be a page where groups are created and named, as well as a page where a user's created groups can be seen
+// perhaps also the groups the user currently belongs to?
 };
 
 default export Groups;
