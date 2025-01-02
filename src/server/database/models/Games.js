907f164dc67a499f951e6efe5670df3152685511
tymey <tyler.meyer.a@gmail.com>
2025-01-01 19:42:55 -0600

    Update Games schema to have five more fields bggId, bestWith, recommendedWith, categories, mechanics

--- src/server/database/models/Games.js
+++ src/server/database/models/Games.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const GameSchema = {
+  bggId: { type: Number },
   name: { type: String, required: true },
   thumbnail: { type: String },
   image: { type: String },
@@ -8,8 +9,12 @@ const GameSchema = {
   yearPublished: { type: Number },
   minPlayers: { type: Number },
   maxPlayers: { type: Number },
+  bestWith: { type: String },
+  recommendWith: { type: String },
   playTime: { type: Number },
   minAge: { type: Number },
+  categories: { type: [String] },
+  mechanics: { type: [String] },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   notes: { type: String, default: '' },
   rating: { type: Number, default: 0 },
