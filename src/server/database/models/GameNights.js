3ec708a0ebab8eb3bbf7a4c4939fa147e71199a8
evan.loria4 <evan.loria4@gmail.com>
2025-01-02 16:30:20 -0600

    Change schema of GameNights collection

--- src/server/database/models/GameNights.js
+++ src/server/database/models/GameNights.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const GameNightSchema = {
   name: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  date: { type: Date, required: false, default: new Date() },
+  date: { type: String },
   time: { type: String },
   isComplete: { type: Boolean, default: false },
   isCancelled: { type: Boolean, default: false },
