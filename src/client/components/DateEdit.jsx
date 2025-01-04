4d9afbbab6269bac4a0a5bc2c7b06baaa66493b7
evan.loria4 <evan.loria4@gmail.com>
2025-01-04 17:04:31 -0600

    PATCH: Dates can be edited within a game night

--- src/client/components/DateEdit.jsx
+++ src/client/components/DateEdit.jsx
@@ -8,9 +8,47 @@ import {
 
 const { useState } = React;
 
-function DateEdit() {
+function DateEdit({
+  gameNight,
+  getGameNights,
+  editingDate,
+  editingTime,
+  setEditingDate,
+  setEditingTime,
+  label,
+}) {
+  // PATCH the date or time in the database
+  const patchEventDate = (element) => {
+    // Grab the key from the element
+    const { key } = element;
+    if (key === 'Enter') {
+    // Grab the id from gameNight and value from the element
+      const { _id } = gameNight;
+      const { value } = element.target;
+      // Set the values of the new fullDate and date for the database using moment
+      const fullDate = moment(value, 'MM/DD/YY').format();
+      const date = moment(value, 'MM/DD/YY').format('dddd, MMMM Do YYYY');
+      // Build the config based on field
+      const config = {
+        newDocument: {
+          fullDate,
+          date,
+        },
+      };
+      // Make axios patch request with the id
+      axios.patch(`/api/game-nights/${_id}`, config)
+        .then(getGameNights)
+        .then(() => { setEditingDate(false); })
+        .catch((err) => {
+          console.error('Error patching the date: ', err);
+        });
+    }
+  };
   return (
     <TextField
+      placeholder={label}
+      helperText="Enter to save"
+      onKeyUp={patchEventDate}
       sx={{
         width: 115,
         '& .MuiInputBase-root': {
@@ -20,5 +58,17 @@ function DateEdit() {
     />
   );
 }
+
+DateEdit.propTypes = {
+  gameNight: PropTypes.shape({
+
+  }).isRequired,
+  getGameNights: PropTypes.func.isRequired,
+  editingDate: PropTypes.bool.isRequired,
+  editingTime: PropTypes.bool.isRequired,
+  setEditingDate: PropTypes.func.isRequired,
+  setEditingTime: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
 
 export default DateEdit;
