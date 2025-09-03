4c18d098d59a8778dc9b97d61b97bec9ddb869e7
RRARI504 <ryansimien@ymail.com>
2025-09-03 17:19:33 -0500

    Add Friend component view that will render the friends components

---
+++ src/client/views/Friend.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+// import ListItemText from '@mui/material/ListItemText';
+// import ListItemButton from '@mui/material/ListItemButton';
+
+export default function Friends() {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
