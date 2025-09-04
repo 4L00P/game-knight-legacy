f2e6e2342ac1fdcb854a0ee997a92cbb552ee0f3
RRARI504 <ryansimien@ymail.com>
2025-09-04 12:39:03 -0500

    Still editing friends list, reverted back to just a div in component

--- src/client/components/group-friends-components/FriendsList.jsx
+++ src/client/components/group-friends-components/FriendsList.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
-
-import ListItemText from '@mui/material/ListItemText';
-import ListItemButton from '@mui/material/ListItemButton';
 
 export default function Friends() {
   return (
-    <ListItemButton component="a" href="#simple-list">
-      <ListItemText primary="Spam" />
-    </ListItemButton>
+    <div>
+      hello
+    </div>
+
   );
 }
