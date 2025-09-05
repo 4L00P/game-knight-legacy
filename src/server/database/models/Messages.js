e698ed350d4754029b9cbc5504c1a8dd7eb5391b
joespaf <josephpfurman@gmail.com>
2025-09-04 19:26:09 -0500

    Messages Model created

--- src/server/database/models/Messages.js
+++ src/server/database/models/Messages.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
-const MessageSchema
+const MessageSchema = {
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  sentAt: { type: 'String' },
+  room: { type: 'String', required: true },
+  text: {type: 'String', required: true }
+};
+
+const Messages = mongoose.model(MessageSchema);
+
+export default Messages;
