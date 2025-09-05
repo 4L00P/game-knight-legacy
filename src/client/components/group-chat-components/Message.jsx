e39d1b029d05931d72a1b03267e77e3528e2f4bd
avaAlley <ava.rose.alley@gmail.com>
2025-09-05 18:40:05 -0500

    Added styling to Chat components. Dynamically renders new messages from form and fake data

--- src/client/components/group-chat-components/Message.jsx
+++ src/client/components/group-chat-components/Message.jsx
@@ -1,1 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+import {
+  Paper,
+} from '@mui/material';
+
+import {
+  styled,
+  alpha,
+} from '@mui/material/styles';
+
+// ------------[MUI THEME]--------------
+const Container = styled(Paper)(({ theme }) => (
+  {
+    alignItems: 'right',
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    padding: theme.spacing(1),
+    width: '8rem',
+    margin: '1rem auto',
+  }
+));
+
+function Message(props) {
+  // --------------[PROPS]---------------
+  const { message } = props;
+
+  // --------------[RENDER]---------------
+  return (
+    <Container elevation={3}>
+      { message }
+    </Container>
+  );
+}
+
+export default Message;
