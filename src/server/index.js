f528fdc32b0c30b98904721b5b823f374869e664
joseph <josephpfurman@gmail.com>
2025-09-06 13:58:34 -0500

    Seperated socket logic into its own file (from index.js to socket.js)

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ require('./database');
 const dotenv = require('dotenv');
 
 const { Server } = require("socket.io");
-
 // Loads the .env information into process.env (Do this before requiring app)
 // This is how we'll hide our .env info
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
@@ -24,24 +23,15 @@ const expressServer = app.listen(PORT, () => {
   }
 });
 
+
 const io = new Server(expressServer, {
   cors: {
     origin: `http://localhost:${PORT}`,
   },
 });
 
-io.on('connection', socket => {
-  console.log(`User ${socket.id} is connected`);
-  socket.join('allChat');
-  socket.to('allChat').emit('joinedNotif', `${socket.id.substring(0, 5)} has joined `)
-
-  socket.on('message', data => {
-    console.log(data);
-    io.to('allChat').emit('message', `${socket.id.substring(0, 5)}: ${data}`);
-  });
-
-  socket.on('joinedRoom', data => {
-    io.to(`${data}`).emit('joinNotif', `${socket.id.substring(0, 5)} has joined ${data}`)
-  });
-});
+module.exports = {
+  io
+}
+require('./socket')
 
--- src/server/socket.js
+++ src/server/socket.js
@@ -1,9 +1,28 @@
-// const WebSocket = require('ws')
-// const server = new WebSocket.Server({port: 9000})
+const { Server } = require("socket.io");
+const { io } = require('./index');
 
-// server.on('connection', socket => {
-//   socket.on('message', message => {
-//     console.log('message in socket', message)
-//     socket.send(`${message}`)
-//   })
-// })
+io.on('connection', socket => {
+
+  // CONNNECTION LOGIC
+  console.log(socket)
+  console.log(`User ${socket.id} is connected`);
+  socket.join('allChat');
+  socket.to('allChat').emit('joinedNotif', `${socket.id.substring(0, 5)} has joined `)
+
+
+  // MESSAGE HANDLING
+  socket.on('message', data => {
+    console.log(data);
+    io.to('allChat').emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+  });
+  
+  // // 
+  // socket.on('joinedRoom', data => {
+  //   io.to(`${data}`).emit('joinNotif', `${socket.id.substring(0, 5)} has joined ${data}`)
+  // });
+
+  // ROLL HANDLING
+  socket.on('roll', (rollData) => {
+
+  });
+});
