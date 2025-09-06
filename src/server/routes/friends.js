28a2efa249cd01a3c699a2167177c770db757ab1
RRARI504 <ryansimien@ymail.com>
2025-09-06 12:38:16 -0500

    Set up get request to get all users in db

--- src/server/routes/friends.js
+++ src/server/routes/friends.js
@@ -7,21 +7,40 @@ const friendsRouter = Router();
 
 // Note: end point (set in app.js) is '/api/friends'
 
+// user can see all friends added
 friendsRouter.get('/', (req, res) => {
-// user can see all the availabilities they entered
-// limit to 1 week?
+  // res.send('hello');
+  Users.find({})
+    .then((user) => {
+      console.log('GET all users from db:', user);
+
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.error('failed to GET users from db', err);
+    });
 });
 
+// user can add friend to list
 friendsRouter.post('/', (req, res) => {
-// user can add an availability
+  // Users.findById()
+  //   .then((user) => {
+  //     console.log('GET all users from db:', user);
+
+  //     res.status(200).send(user);
+  //   })
+  //   .catch((err) => {
+  //     console.error('failed to GET users from db', err);
+  //   });
+
 });
 
 friendsRouter.delete('/', (req, res) => {
-// user can delete an availability
+  // user can delete a friend from the list
 });
 
 friendsRouter.patch('/', (req, res) => {
-// change the availability to whichever day/start/end time user wants
+  // user can check if a friend request is pending.
 });
 
 module.exports = {
