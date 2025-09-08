897ba2147f775c787dfcbddaacb018201f98ff9c
RRARI504 <ryansimien@ymail.com>
2025-09-07 22:57:17 -0500

    Began setting up logic for checking if a friend request is pending

--- src/server/routes/pending.js
+++ src/server/routes/pending.js
@@ -3,8 +3,20 @@ const { Pending } = require('../database');
 
 const pendingFriendsRouter = Router();
 
-pendingFriendsRouter.get('/', (req, res) => {
+pendingFriendsRouter.get('/:requester:recipient', (req, res) => {
   // user can check if a friend request is pending.
+  let { requester, recipient } = req.params;
+  requester = requester.slice(1);
+  recipient = recipient.slice(1);
+
+  Pending.find({
+    email: search,
+  }).then((user) => {
+    console.log('GET all users from db:', user);
+    res.status(200).json(user);
+  }).catch((err) => {
+    console.error('failed to GET users from db', err);
+  });
 
 });
 
